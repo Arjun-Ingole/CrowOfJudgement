86c827bc2155c572a74ffb7c734141afa60f3f56
Arjun-Ingole <arjuningole38@gmail.com>
2022-08-20 07:45:17 +0530

    Crow: bring back regex
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ while True:
         tweetid = i.id
 
     if tweetid not in toreply:
-        cleaned_prompt = prompt
-        cleaned_prompt = cleaned_prompt.replace(hashtag, '').split()
+        cleaned_prompt = cleaned_prompt = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ", prompt).split())
+        cleaned_prompt = cleaned_prompt.replace(hashtag, '')
 
         def generateResponse():
             response = openai.Completion.create(
